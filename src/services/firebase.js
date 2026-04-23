d81affa2fa235561cd1fe4836bd1ee1b55892aee
leetureview <leetureview@gmail.com>
2026-04-23 14:39:07 +0700

    Add Firebase SDK configuration

---
+++ src/services/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics, isSupported as isAnalyticsSupported } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyApEq1aZ60eCJOXzrz0p3mqMip96ibyy18',
+    authDomain: 'quanlybuondon.firebaseapp.com',
+    projectId: 'quanlybuondon',
+    storageBucket: 'quanlybuondon.firebasestorage.app',
+    messagingSenderId: '684982209167',
+    appId: '1:684982209167:web:3a5a9599f9fe84a930befa',
+    measurementId: 'G-VWZ9F7YSC3',
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+
+export const analytics = isAnalyticsSupported().then((ok) =>
+    ok ? getAnalytics(app) : null
+);
